689b0f3daff79ac425ab8eac150fb6a0352ef807
bsnacks1 <bsnacks@users.noreply.github.com>
2020-04-23 02:13:34 -0400

    Update to project settings as well as GameObject modifications

---
+++ Assets/Scripts/Core/AutoSpriteSorter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+using UnityEngine;
+public class AutoSpriteSorter : MonoBehaviour
+{
+
+    SpriteRenderer tempRend;
+
+    void Awake()
+    {
+
+        tempRend = GetComponent<SpriteRenderer>();
+
+    }
+
+    void Update()
+    {
+
+        tempRend.sortingOrder = (int)Camera.main.WorldToScreenPoint(this.transform.position).y * -1;
+
+    }
+}

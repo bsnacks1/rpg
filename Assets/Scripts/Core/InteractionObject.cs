ebda905ee6a844ea9e09af5978fb00f7d659f3a0
bsnacks1 <bsnacks@users.noreply.github.com>
2020-04-24 07:31:45 -0400

    Added two new scripts for player interactions. Code is finnicky so be careful with it.

---
+++ Assets/Scripts/Core/InteractionObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionObject : MonoBehaviour
+{
+
+    public void DoInteraction()
+
+    {
+        gameObject.SetActive(false);
+
+    }
+}
